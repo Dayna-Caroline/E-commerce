03e3bceb735ee118c05e9f281619c8e9f2a9a282
Dayna-Caroline <dayna.caroline@unesp.br>
2020-09-10 07:50:30 -0300

    Nomeei uma função no login_e_cadastro

--- back/login_e_cadastro.js
+++ back/login_e_cadastro.js
@@ -1,3 +1,4 @@
+//Efeito da aba---------------------------------------------------------
 document.querySelector('.img-btn').addEventListener('click', function()
 	{
 		document.querySelector('.cont').classList.toggle('s-signup')
