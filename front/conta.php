640ca61e533835c18f54bcd7bee517df98f7ebd0
Dayna-Caroline <dayna.caroline@unesp.br>
2020-09-14 14:48:23 -0300

    Criei a página de conta

---
+++ front/conta.php
@@ -0,0 +1,180 @@
+<!--Dayna Caroline - N°11 - Criação: 02/09-->
+<!--Augusto Creppe - N°06 e Dayna Caroline - N°11 - Atualização: 09/09-->
+
+<!DOCTYPE html>
+
+<?php
+    $logado = null;
+
+    session_start();
+    
+    if(isset($_SESSION['email']))
+    {
+        $logado = $_SESSION['email'];
+        $adm = $_SESSION['adm'];
+        $nome = $_SESSION['nome']; 
+        $sexo = $_SESSION['sexo'];
+    }
+?>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cup&Mug</title>
+        <link rel="stylesheet" href="../styles/conta.css">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    </head>
+
+    <body>
+        <div class="tudo">
+
+            <div class="topo">
+                <div class="header">
+                    <div class="container">
+                        <div class="navbar">
+                            <div class="logo">
+                                <img src="../imgs/index/melhor.jpg" alt="" width="100px" heigth="100px">
+                            </div>
+                            <nav>
+                                <ul id="MenuItems">
+                                    <li><a href="../index.php">Home</a></li>
+                                    <li><a href="../front/produtos.php">Produtos</a></li>
+                                    <li><a href="">Sobre</a></li>
+                                </ul>
+                            </nav>
+                            <a href="
+                            
+                            <?php
+                                if($logado ==  null)
+                                    echo "../front/login_e_cadastro.php";
+                                else
+                                //echo " LINK DA PAGINA DA CONTA ";
+                            ?>
+                            
+                            "><img src="../imgs/index/conta.png" alt="" width="30px" heigth="30px"></a>
+                            <a href="
+                            
+                            <?php
+                                if($logado ==  null)
+                                    echo "../front/login_e_cadastro.php";
+                                else
+                                  //echo " LINK DA PAGINA DO CARRINHO ";
+                            ?>
+                            
+                            "><img src="../imgs/index/carrinho.png" alt="" width="30px" heigth="30px"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="internas">
+                <!--Conta - Informações-------------------------------------------------------------------------------------------------------->
+                <div class="small-container">
+                    <h2>Informações da conta</h2>
+                    <label>
+                        <div class="icon tel"><i class="fas fa-user"></i></div>
+                        <p>Nome</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon tel"><i class="fas fa-user"></i></div>
+                        <p>Sobrenome</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon"><i class="fas fa-venus-mars"></i></div>
+                        <p>Sexo</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon tel"><i class="fas fa-calendar-alt"></i></div>
+                        <p>Data de Nascimento</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon"><i class="fas fa-address-card"></i></div>
+                        <p>CPF</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon"><i class="fas fa-map-marked-alt"></i></div>
+                        <p>CEP</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon tel"><i class="fas fa-mobile"></i></div>
+                        <p>Telefone</p>
+                    </label>
+                        
+                    <label>
+                        <div class="icon"><i class="fas fa-envelope-open-text"></i></div>
+                        <p>Email</p> 
+                    </label>
+                        
+                    <label>
+                        <div class="icon"><i class="fas fa-lock"></i></div>   
+                        <p>Senha</p>
+                    </label>
+
+                    <div class="botoes">
+                        <button>Alterar informação</button>
+                        <button>Ver histórico</button>
+                        <button><a href="../back/logout.php">Sair</a></button>
+                    </div>
+                    
+                </div>   
+            </div> <!--Internas-->
+
+            <div class="rodape">
+                
+                <!--Footer-------------------------------------------------------------------------------------------------------------------->
+                    <div class="footer">
+                        <div class="navbar">
+                            <section>
+                                <ul id="MenuItems">
+                                    <li><a href="../index.php">Home</a></li>
+                                    <li><a href="../front/podutos.php">Produtos</a></li>
+                                    <li><a href="">Sobre</a></li>
+                                </ul>
+                                <a href="
+                                
+                                <?php
+                                    if($logado ==  null)
+                                        echo "../front/login_e_cadastro.php";
+                                    else
+                                    //echo " LINK DA PAGINA DA CONTA ";
+                                ?>
+                                
+                                "><img src="../imgs/index/conta_branco.jpg" alt="" width="30px" heigth="30px"></a>
+                                <a href="
+                                
+                                <?php
+                                    if($logado ==  null)
+                                        echo "../front/login_e_cadastro.php";
+                                    else
+                                    //echo " LINK DA PAGINA DO CARRINHO ";
+                                ?>
+                                
+                                "><img src="../imgs/index/carrinho_branco.jpg" alt="" width="30px" heigth="30px"></a>
+                            </section>
+                        </div>
+                            
+                        <div class="footer-col-1">
+                            <ul>
+                                <li>03 - Ana Júlia,</li>
+                                <li>06 - Augusto Creppe,</li>
+                                <li>11 - Dayna Caroline,</li>
+                                <li>19 - João Gabriel,</li>
+                                <li>20 - João Pedro,</li>
+                                <li>28 - Maria Isabel</li>
+                            </ul>
+                        </div>
+
+                        <div class="inicio">
+                            <a href="carrinho.php">Voltar ao inicio</a>
+                        </div>
+                    </div>
+            </div>                  
+        </div>
+    </body>
+</html>
