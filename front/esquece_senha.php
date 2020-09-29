74cf0466bc16e58d412915d3e5ffecffa7513ba1
Dayna-Caroline <dayna.caroline@unesp.br>
2020-09-29 08:18:05 -0300

    Coloquei o front q a Mabel fez

--- front/esquece_senha.php
+++ front/esquece_senha.php
@@ -1,19 +1,142 @@
 <!DOCTYPE html>
 
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Empresa</title>
-    
-    <link rel="stylesheet" href="./styles/index.css">
-</head>
+<?php
+    include "../back/conexao.php";
+    $logado = null;
 
-<body>
-    <h2 class="titulo titulo-segundo">Digite o email</h2>
-    <form action="../back/envia_email.php" method="post" class="form">
-        <input type="email" name="email" placeholder="Email" required>
-        <button type="submit" class="btn btn-segundo">Enviar</button>  
-    </form>   
-</body>
+    session_start();
+    
+    if(isset($_SESSION['email']))
+    {
+        $logado = $_SESSION['email'];
+        $adm = $_SESSION['adm'];
+        $nome = $_SESSION['nome']; 
+        $sexo = $_SESSION['sexo'];
+        $id_user = $_SESSION['id_user'];
+    }
+?>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cup&Mug</title>
+        <link rel="stylesheet" href="../styles/rec_senha.css">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    </head>
+
+    <body>
+        <div class="tudo">
+
+            <div class="topo">
+                <div class="header">
+                    <div class="container">
+                        <div class="navbar">
+                            <div class="logo">
+                                <a href="../index.php"><img src="../imgs/tudo/melhor.JPG" alt="" width="100px" heigth="100px"></a>
+                            </div>
+                            <nav>
+                                <ul id="MenuItems">
+                                    <li><a href="../index.php">Home</a></li>
+                                    <li><a href="../front/produtos.php">Produtos</a></li>
+                                    <li><a href="../front/sobre.php">Sobre</a></li>
+                                </ul>
+                            </nav>
+                            <a href="
+                            <?php
+                                if($logado ==  null)
+                                    echo "../front/login_e_cadastro.php";
+                                else
+                                    echo "../front/conta.php";
+                            ?>
+                            
+                            
+                            "><img src="../imgs/tudo/conta.png" alt="" width="30px" heigth="30px"></a>
+                            <a href="
+                            <?php
+                                if($logado ==  null)
+                                    echo "../front/login_e_cadastro.php";
+                                else
+                                    echo "../front/conta.php";
+                            ?>
+
+                            
+                            
+                            "><img src="../imgs/tudo/carrinho.png" alt="" width="30px" heigth="30px"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="internas">
+                <div class="small-container">
+                     <div class=form>
+                        <h2>Esqueceu a senha</h2>
+                        <form action="../back/envia_email.php" method="post" class="form">
+                            <label>Digite o email para recuperação da senha:
+                                <br><br>
+                                <input type="email" name="email" placeholder="Email">
+                            </label>
+                            <br><br><br>
+                            <div class="botoes">
+                                <button type="reset">Limpar</button>
+                                <button type="submit">Enviar</button>
+                            </div>
+                        </form>
+                         
+                    </div>
+                </div>    
+            </div> <!--Internas-->
+
+            <div class="rodape">
+                
+                <!--Footer-------------------------------------------------------------------------------------------------------------------->
+                    <div class="footer">
+                        <div class="navbar">
+                            <section>
+                                <ul id="MenuItems">
+                                    <li><a href="../index.php">Home</a></li>
+                                    <li><a href="../front/produtos.php">Produtos</a></li>
+                                    <li><a href="../front/sobre.php">Sobre</a></li>
+                                </ul>
+                                <a href="
+                                <?php
+                                    if($logado ==  null)
+                                        echo "../front/login_e_cadastro.php";
+                                    else
+                                        echo "../front/conta.php";
+                                ?>
+                                
+                                
+                                "><img src="../imgs/tudo/conta_branco.JPG" alt="" width="30px" heigth="30px"></a>
+                                <a href="
+                                <?php
+                                    if($logado ==  null)
+                                        echo "../front/login_e_cadastro.php";
+                                    else
+                                        echo "../front/carrinho.php";
+                                ?>
+                                
+                                
+                                "><img src="../imgs/tudo/carrinho_branco.JPG" alt="" width="30px" heigth="30px"></a>
+                            </section>
+                        </div>
+                            
+                        <div class="footer-col-1">
+                            <ul>
+                                <li>03 - Ana Júlia,</li>
+                                <li>06 - Augusto Creppe,</li>
+                                <li>11 - Dayna Caroline,</li>
+                                <li>19 - João Gabriel,</li>
+                                <li>20 - João Pedro,</li>
+                                <li>28 - Maria Isabel</li>
+                            </ul>
+                        </div>
+
+                        <div class="inicio">
+                            <a href="conta.php">Voltar ao inicio</a>
+                        </div>
+                    </div>
+            </div>                  
+        </div>
+    </body>
 </html>
