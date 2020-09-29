3d810f9f10c14b026b87dc5ad21fd40d1d5341aa
Dayna-Caroline <dayna.caroline@unesp.br>
2020-09-29 08:25:07 -0300

    Arrumei o front do mensagem do email

--- front/mensagem_email.php
+++ front/mensagem_email.php
@@ -6,12 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E-mail enviado</title>
     
-    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="../styles/index.css">
 </head>
 
 <body>
-    <h2 class="titulo titulo-segundo">Email enviado com sucesso </h2>
-    <a href="login_e_cadastro.php">Voltar a página de login</a>
+    <center>
+        <h2 class="titulo titulo-segundo">Consulte seu email e pegue sua nova senha</h2>
+        <br><br>
+        <h4>Sugerimos que após a entrada você altere sua senha.</h4>
+        <br><br>
+        <a href="login_e_cadastro.php">Voltar a página de login &#8594;</a>
+    </center>
 </body>
     
 </html>
