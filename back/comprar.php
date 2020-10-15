cde1c0b8aab346685ca3a694a34b213544fbeb92
JoaoGabrielNoce <70067686+JoaoGabrielNoce@users.noreply.github.com>
2020-10-15 17:52:54 -0300

    Adicionei envio de email ao fim da compra

--- back/comprar.php
+++ back/comprar.php
@@ -1,4 +1,12 @@
 <?php
+    require_once('src/PHPMailer.php');
+    require_once('src/SMTP.php');
+    require_once('src/Exception.php');
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
     include "conexao.php";
 
     $logado = null;
@@ -19,8 +27,26 @@
 
     if($qtde > 0)
     {
-        $sucesso = true;
+        //PEGA O PREÇO
+        $soma_total=0;
+        $sql="SELECT c.id_user, c.id_produto, c.quantidade, c.excluido,p.id_produto, p.produto, p.descricao, p.preco, p.imagem FROM carrinho AS c JOIN produto AS p ON c.id_produto = p.id_produto WHERE c.id_user = $id_user AND c.excluido = FALSE;";                       
+        $resultado = pg_query($conecta, $sql);
+        $qtdeA = pg_num_rows($resultado);
 
+        if($qtdeA > 0)
+        {
+
+            for($cont=0; $cont < $qtde; $cont++)
+            {
+                $linha=pg_fetch_array($resultado);
+                $estoque=0;
+                for($cont2=0; $cont2 < $linha['quantidade']; $cont2++)
+                    $soma_total+=$linha['preco'];
+
+            }
+        }
+        $sucesso = true;
+        
         //SALVAR COMPRA
         $date = date("Y/m/d");
         $sql = "INSERT INTO compra VALUES(DEFAULT, $id_user, '$date', FALSE);";
@@ -107,11 +133,54 @@
             $sucesso = false;
             echo "erro ao efetuar compra aqui! 3";
         }
-
+        
         if($sucesso == true)
         {
-            header("Location: ./gerar_pdf.php");
+            
+            //PEGA O CEP
+            $cep = 0;
+            $sql="SELECT cep FROM usuario WHERE id_user=$id_user;";
+            $resultado = pg_query($conecta, $sql);
+            $qtdeB = pg_num_rows($resultado);
+
+            if($qtdeB > 0)
+            {
+                $linha=pg_fetch_array($resultado);
+
+                $cep=$linha['cep'];
+            }
+            //ENVIA EMAIL   
+            $mail = new PHPMailer(true);
+
+            try {
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'ecommerce.grupo6@gmail.com';
+                $mail->Password = 'jovita123';
+                $mail->Port = 587;
+
+                $mail->setFrom('ecommerce.grupo6@gmail.com');
+                $mail->addAddress($logado);
+
+
+                $mail->isHTML(true);
+                $mail->Subject = 'Confirmar sua compra Cup&Mug';
+                $mail->Body = 'Sua compra no valor de '.$soma_total.' reais foi bem sucedida e será entregue no endereço do CEP: '.$cep;
+                $mail->AltBody = 'Sua compra no valor de '.$soma_total.' reais foi bem sucedida e será entregue no endereço do CEP: '.$cep;
+
+                if($mail->send()) {
+                    echo 'Email enviado com sucesso';
+                } else {
+                    echo 'Email nao enviado';
+                }
+            } catch (Exception $e) {
+                echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+            }
+            ?><meta http-equiv="refresh" content="0; URL='./gerar_pdf.php'"/><?php
         }
+
         else
         {
             echo '<script language="javascript">';
