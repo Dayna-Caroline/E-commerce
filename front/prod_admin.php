5047080576b5b7dcad00988fd726bce1907b4b69
Dayna-Caroline <dayna.caroline@unesp.br>
2020-10-26 08:41:31 -0300

    Adicionei o front de prod_admin

--- front/prod_admin.php
+++ front/prod_admin.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Cup&Mug</title>
-    <link rel="stylesheet" href="../styles/graficos.css">
+    <link rel="stylesheet" href="../styles/prod_admin.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 </head>
 
@@ -48,7 +48,85 @@
         </div>
 
         <div class="internas">
-        
+                    <div class="row row-2">
+                            <form class="pesq_text" action="../front/pesq_user.php" method="post">
+                                <input type="text" name="pesq" class="text" placeholder="Pesquisa">
+                                <button type="submit" class="icon">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                            </form>
+
+                            <div class="opcoes">
+                                <input type="radio" name="opcao" value="produto" checked> Produto
+                                <input type="radio" name="opcao" value="material"> Material
+                                <input type="radio" name="opcao" value="tamanho"> Tamanho
+                                <input type="radio" name="opcao" value="estoque"> Estoque
+                            </div>
+            
+                    </div>
+
+
+            <div class="small-container">
+                        <div class="tabela">
+                            <div class="titulos">
+                                <div class="produto">Produto</div>
+                                <div class="quant">Material</div>
+                                <div class="quant">Tamanho</div>
+                                <div class="quant">Estoque</div>
+                                <div class="preco">Preço</div>
+                            </div>
+
+                            <?php
+                                $sql="SELECT *FROM produto WHERE excluido='false' ORDER BY id_produto;";
+                                
+                                $resultado = pg_query($conecta, $sql);
+                                $qtde = pg_num_rows($resultado);
+                                
+                                if($qtde > 0)
+                                {
+
+                                    for($cont=0; $cont < $qtde; $cont++)
+                                    {
+                                        $linha=pg_fetch_array($resultado);
+
+                                        echo "
+                                        <div class='produtos'>
+                                            <div class='produto completa'>
+                                                <img src='..".$linha['imagem']."' width='50px'>
+                                                <div class='descricao'>
+                                                    <br>
+                                                    <p>".$linha['produto']."</p>
+                                                    <a href='../back/remove_prod_carrinho.php?id_prod=".$linha['id_produto']."'>Mais detalhes</a>
+                                                </div>
+                                            </div>
+
+                                            <div class='quant'>
+                                                <span>".$linha['material']."</span>
+                                            </div>
+
+                                            <div class='quant'>
+                                                <span>".$linha['tamanho']."mL</span>
+                                            </div>
+
+                                            <div class='quant'>
+                                                <span>".$linha['quantidade']."</span>
+                                            </div>
+
+                                            <div class='preco1'>
+                                                <span>R$".$linha['preco'].",00</s´pan>
+                                            </div>
+                                        </div>";
+                                    }
+                                }
+                                else
+                                {
+                                    echo "<center><br><br><br><br><br><br><br><br><br><br><br><br><h1><div class='tabela'><b> Seu carrinho está vazio </b></div></h1> <br><br><br><br><br><br><br></center>";
+                                }
+
+                            ?>
+                        </div> 
+                    
+                </div> 
         </div> 
 
         <div class="rodape">
